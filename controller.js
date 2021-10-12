7e95b8f7b8b3ef7c5e4b894ed6c1d117a36d9242
kevrey <kevinreyhan49@gmail.com>
2021-10-12 15:22:36 +0700

    menambahkan edit controller

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("aplikasi API berjalan")
+    response.ok("aplikasi API berjalan",res)
 };
