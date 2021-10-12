f90dcf91dfc91dee5a6cf58ce86db0d23525b1cf
kevrey <kevinreyhan49@gmail.com>
2021-10-12 15:54:15 +0700

    menampilkan data

--- controller.js
+++ controller.js
@@ -6,3 +6,15 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     response.ok("aplikasi API berjalan",res)
 };
+
+// menampilkan semua data 
+exports.tampilkandata = function(req,res){
+    connection.query('SELECT * FROM passion', function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }
+        else{
+            response.ok(rows, res)
+        }
+    });
+};
